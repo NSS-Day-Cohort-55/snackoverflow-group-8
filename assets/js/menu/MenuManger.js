6c81631e3a44dace7ca8d417797df078d882cd4b
karlarn <karlarnichols@gmail.com>
2022-03-25 11:04:43 -0500

    Filter through menu based on the food type criteria#1

--- assets/js/menu/MenuManger.js
+++ assets/js/menu/MenuManger.js
@@ -1,6 +1,16 @@
 const apiURL = "http://localhost:8088";
 
-export const getFoods = () => {
+export const getFoods = (id) => {
+    if (id===0){
     return fetch(`${apiURL}/foods`)
-    .then(response => response.json())
+    .then(response => response.json())}
+    else {
+        return fetch(`${apiURL}/foods?menuId=${id}`)
+            .then(response=>response.json())
+    }
 }
+
+// export const getFoodsByMenuId=(id)=>{
+//     return fetch(`${apiURL}/foods?menuId=${id}`)
+//     .then(response=>response.json())
+// }
